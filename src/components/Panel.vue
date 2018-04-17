965f5ba5e7345bad449022a2462ed9779c1ce758
LoliMay <1404363070@qq.com>
2018-04-17 21:45:06 +0800

    初始化

---
+++ src/components/Panel.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-button type="primary">测试</el-button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '欢迎来到你的第一个Vue应用'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
